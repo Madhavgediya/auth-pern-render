d0e05c98e7f9b7d713c43bbcfffb41892de3c814
Madhavgediya <madhavgediya1@gmail.com>
2026-02-25 11:50:34 +0530

    Some changes for live server

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,10 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "https://mg-pern-auth-project-backend.onrender.com/api",
-  baseURL:
-    "http://localhost:5000/api" ||
-    "https://authentication-pern-render.onrender.com/api" ||
-    "https://mg-pern-auth-project-backend.onrender.com/api",
+  baseURL: "https://mg-pern-auth-project-backend.onrender.com/api",
   withCredentials: true, // MUST be true
 });
 
