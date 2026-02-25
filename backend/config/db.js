8f03b662cee1059a54496c66d6912040071225d1
Madhavgediya <madhavgediya1@gmail.com>
2026-02-25 11:45:34 +0530

    Some changes for live

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,22 +4,22 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 // Local Database Connection
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-});
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_NAME,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+// });
 
 // Remote Database Connection (NeonDB)
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl:
-//     process.env.NODE_ENV === "production"
-//       ? { rejectUnauthorized: false }
-//       : false,
-// });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
+});
 
 pool.on("connect", () => {
   console.log("Connected to the Database...!");
