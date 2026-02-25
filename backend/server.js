3ba49382462774e75a9426f5612e00276b0e6448
Madhavgediya <madhavgediya1@gmail.com>
2026-02-25 12:01:24 +0530

    Some changes for live server

--- backend/server.js
+++ backend/server.js
@@ -7,19 +7,18 @@ const authRoutes = require("./routes/auth.js");
 const app = express();
 
 app.use(express.json());
-// app.use(
-//   cors({
-//     origin:
-//       process.env.CLIENT_URL || "https://mg-pern-auth-project.vercel.app",
-//     credentials: true,
-//   }),
-// );
 app.use(
   cors({
-    origin: "https://auth-pern-render.onrender.com",
+    origin: process.env.CLIENT_URL || "https://auth-pern-render.onrender.com",
     credentials: true,
   }),
 );
+// app.use(
+//   cors({
+//     origin: "https://auth-pern-render.onrender.com",
+//     credentials: true,
+//   }),
+// );
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "https://mg-pern-auth-project-backend.onrender.com/api",
-  baseURL: "https://mg-pern-auth-project-backend.onrender.com/api",
+  baseURL: "https://authentication-pern-render.onrender.com/api",
   withCredentials: true, // MUST be true
 });
 
